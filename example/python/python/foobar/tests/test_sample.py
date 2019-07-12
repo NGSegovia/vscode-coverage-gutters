877609a2d84d5ed65b2585ac35dbd2ec65cede73
Nacho Garcia Segovia <nacho.gs@zaleos.net>
2019-07-12 11:25:02 +0200

    Dummy change

--- example/python/python/foobar/tests/test_sample.py
+++ example/python/python/foobar/tests/test_sample.py
@@ -7,8 +7,8 @@ from foo.a import identity
 #    """test adding"""
 #    assert func(4) == identity(5)
 
-#def test_fun():
-#    assert func(4) == 5
+def test_fun():
+    assert func(4) == 5
 
 def test_identity():
     assert identity(5) == 5
