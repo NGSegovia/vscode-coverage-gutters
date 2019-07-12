40515b8e760c88774f8b10bb1953665d2df56980
Nacho Garcia Segovia <nacho.gs@zaleos.net>
2019-07-12 10:49:39 +0200

    Dummy change

--- example/python/python/foobar/tests/test_sample.py
+++ example/python/python/foobar/tests/test_sample.py
@@ -3,6 +3,12 @@ from bar.a import func
 from foo.a import identity
 
 
-def test_answer():
-    """test adding"""
-    assert func(4) == identity(5)
+#def test_answer():
+#    """test adding"""
+#    assert func(4) == identity(5)a
+
+#def test_fun():
+#    assert func(4) == 5
+
+def test_identity():
+    assert identity(5) == 5
